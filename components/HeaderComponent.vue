7db18d51f386220ef0b3c908c8bf4d73d1e127d1
Drivakos <ciedas99@gmail.com>
2023-09-24 23:33:26 +0300

    Feat: Added scss support

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -8,12 +8,3 @@
   </div>
 </template>
 
-<style scoped>
-  .header-container {
-    background-color: #f5f5f5;
-    padding: 20px;
-    border-radius: 5px;
-    margin-bottom: 20px;
-    font-size: clamp(12px, 2vw, 20px);
-  }
-</style>
